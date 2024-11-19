a5b7e55fd2e6128cdb88f37e9b5743b0e5bb1670
bil-glitch <bilelzayeni5@gmail.com>
2024-11-19 21:23:02 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
-basic.forever(function () {
-	
+let choix = 0
+let adversaire = ""
+let joueur = ""
+input.onButtonPressed(Button.A, function () {
+    choix = randint(0, 2)
+    if (choix == 0) {
+        adversaire = "Pierre"
+    } else if (choix == 1) {
+        adversaire = "Papier"
+    } else {
+        adversaire = "Ciseaux"
+    }
+    if (adversaire == "Ciseaux") {
+        joueur = "Pierre"
+    } else {
+        joueur = ["Pierre", "Papier", "Ciseaux"][randint(0, 2)]
+    }
+    basic.showString("Adv: " + adversaire)
+    basic.pause(500)
+    basic.showString("Jou: " + joueur)
+    if (joueur == "Pierre" && adversaire == "Ciseaux" || joueur == "Papier" && adversaire == "Pierre" || joueur == "Ciseaux" && adversaire == "Papier") {
+        basic.showString("Gagne!")
+    } else if (joueur == adversaire) {
+        basic.showString("Egalite!")
+    } else {
+        basic.showString("Perdu!")
+    }
 })
